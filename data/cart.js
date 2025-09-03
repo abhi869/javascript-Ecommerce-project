3808f06c5c40c58bb4270a494aee917e07539358
abhi869 <abhinavgaur545@gmail.com>
2025-09-03 15:22:51 +0200

    Saving cart using localstorage

--- data/cart.js
+++ data/cart.js
@@ -1,10 +1,19 @@
-export let cart = [{
+export let cart = JSON.parse(localStorage.getItem('cart'))
+
+if(!cart){
+  cart=[{
     productId : "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
     quantity : 2
 },{
     productId : "15b6fc6f-327a-4ec4-896f-486349e85a3d",
     quantity : 1
 }];
+}
+
+
+ function addLocalStorage(){
+    localStorage.setItem('cart', JSON.stringify(cart))
+ }
 
   export function addToCart(productId){
          let matchingName;
@@ -29,6 +38,8 @@ export let cart = [{
                     quantity : quantitySelect
                 })
             }
+
+            addLocalStorage();
      }
 
     export function removeCart(productId){
@@ -40,4 +51,6 @@ export let cart = [{
             }
         })
         cart = newCart;
+
+        addLocalStorage();
      }
